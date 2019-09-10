f41109aa1d327cb5feb6bd3567082d7da5123665
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2019-09-09 23:45:15 -0400

    adicionando bibliotecas php na pasta util

--- app/controllers/registrarCtrl.js
+++ app/controllers/registrarCtrl.js
@@ -3,12 +3,24 @@ angular.module(module).controller('registrarCtrl', function ($rootScope, $scope,
     if ($rootScope.usuario) { $location.path("/home"); return false; }
 
     $scope.obj = {
+        nome: null,
         email: null,
         senha1: null,
         senha2: null
     }
 
-   $scope.salvar = function (obj) {
-        console.log(obj);
+    $scope.confirmar = false;
+
+    $scope.salvar = function (obj) {
+        let copy = angular.copy(obj);
+        if (copy.senha != copy.senha2) {
+            return SweetAlert.swal({ html: true, title: "Atenção", text: "As senhas estão diferentes!", type: "error" });
+        }
+
+        if (!$scope.confirmar) {
+            $scope.confirmar = true;
+        }else{
+            console.log(copy);
+        }
    }
 });
