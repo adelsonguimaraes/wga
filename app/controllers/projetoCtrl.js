ae5babf0c97977bcc9b70e861bd248f0d5c4eb94
Adelson Guimarães <adelsonguimaraes@gmail.com>
2019-09-17 15:42:58 -0400

    adição de fundo em vídeo

--- app/controllers/projetoCtrl.js
+++ app/controllers/projetoCtrl.js
@@ -4,4 +4,19 @@ angular.module(module).controller('projetoCtrl', function ($rootScope, $scope, a
     
     $scope.titulo = 'Projetos';
     
+    $scope.projetos = [
+        {
+            id: 1,
+            nome: 'Grupo Movus',
+            descricao: 'Agrupamento de todos os projetos relativos ao grupo Movus',
+            datacadastro: '2019-08-10 10:00:00'
+        },
+        {
+            id: 2,
+            nome: 'Câmara Municipal',
+            descricao: 'Agrupamento de projetos realizados com a Câmara Municipal de Manaus',
+            datacadastro: '2019-06-08 10:00:00'
+        }
+    ];
+
 });
